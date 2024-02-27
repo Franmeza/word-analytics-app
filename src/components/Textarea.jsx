8cc6ae99044281f1387344327d75aba0c95979bf
Franmeza <ing.meza.francisco@gmail.com>
2024-02-27 14:19:15 -0500

    logic for textarea section

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 function Textarea() {
-  return <textarea className="textarea"></textarea>;
+  const [text, setText] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      newText = newText.replace("<script>", "");
+      setShowWarning(true);
+    } else {
+      setShowWarning(false);
+    }
+
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      ></textarea>
+      {showWarning && <div className="warning">No script tag allowed!</div>}
+    </div>
+  );
 }
 
 export default Textarea;
