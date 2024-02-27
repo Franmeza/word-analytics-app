86929edf36019383d05b374562f32d6efbeb74aa
Franmeza <ing.meza.francisco@gmail.com>
2024-02-27 11:32:01 -0500

    App structure

---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import Stats from "./Stats";
+import Textarea from "./TextArea";
+
+function Container() {
+  return (
+    <main>
+      <Textarea />
+      <Stats />
+    </main>
+  );
+}
+
+export default Container;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <small>Created by Francisco Meza</small>
+    </footer>
+  );
+}
+
+export default Footer;
